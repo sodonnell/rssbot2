e45dedefba5504221417cd8a4b1f09bf51dee33f
Sean O'Donnell <sean@seanodonnell.com>
2019-06-28 00:09:51 -0700

    another format test

--- rssbot2.py
+++ rssbot2.py
@@ -45,7 +45,7 @@ class rssbot2:
         return self.conn
 
     def get_feeds(self):
-        sql = format("SELECT title, link, id FROM rssbot2_feeds WHERE active = 'Y' ORDER BY RAND() LIMIT 0, {} ", end='')
+        sql = "SELECT title, link, id FROM rssbot2_feeds WHERE active = 'Y' ORDER BY RAND() LIMIT 0, {} ".format(self.max_feeds)
         cursor = self.conn.cursor()
         cursor.execute(sql, self.max_feeds)
         self.feeds = cursor.fetchall()
