00a2678b692aed3562c22147143c0b1953b32fcf
Sean O'Donnell <sean@seanodonnell.com>
2019-09-14 12:41:43 -0700

    runner exception processing update.

--- runner.py
+++ runner.py
@@ -39,30 +39,30 @@ if rssbot.feeds_count > 0:
         else: 
             http_status = "HTTP Response Status Code: %d" % (rss.status)
             print(http_status)
-        if rss.feed.has_key('title'):
-            if rssbot.debug:
-                print("--------------------------")
-                print("RSS Feed Title: {}". format(rss.feed.title))
-    
-            if len(rss.entries) > 0:
-                for entry in rss.entries:
-                    if entry.has_key('title') and entry.has_key('link'):
-    
-                        id = rssbot.add_link(feed[2],entry.title,entry.link)
+            if rss.feed.has_key('title'):
+                if rssbot.debug:
+                    print("--------------------------")
+                    print("RSS Feed Title: {}". format(rss.feed.title))
+        
+                if len(rss.entries) > 0:
+                    for entry in rss.entries:
+                        if entry.has_key('title') and entry.has_key('link'):
+        
+                            id = rssbot.add_link(feed[2],entry.title,entry.link)
 
-                        try:
-                            if id > 0:
-                                if rssbot.debug:
-                                    print("\nAdded Record {}". format(id))
-                                    print(str(entry.title))
-                                    print(str(entry.link))
-                                k=k+1
-                        except:
-                            print("Exception thrown while processing item.")
+                            try:
+                                if id > 0:
+                                    if rssbot.debug:
+                                        print("\nAdded Record {}". format(id))
+                                        print(str(entry.title))
+                                        print(str(entry.link))
+                                    k=k+1
+                            except:
+                                print("Exception thrown while processing item.")
 
-                        j=j+1
-        else:
-            rssbot.deactivate_feed(feed[2])
+                            j=j+1
+            else:
+                rssbot.deactivate_feed(feed[2])
             
         i=i+1
 
