36777c102a2aae354cd2b874de879d8fa745c59e
Sean O'Donnell <sean@seanodonnell.com>
2019-12-07 12:06:25 -0800

    reverted to imparitive style

--- runner.py
+++ runner.py
@@ -5,65 +5,64 @@ from rssbot2 import rssbot2
 
 rssbot = rssbot2()
 
-def parse_feeds(rssbot):
 
-    rssbot.db_connect()
-    rssbot.get_feeds()
+rssbot.db_connect()
+rssbot.get_feeds()
 
-    print(banner.rssbot_logo)
-    print("--------------------------")
-    print("Title: {}". format(rssbot.title))
-    print("Agent: {}". format(rssbot.user_agent))
-    print("Max Feeds: {}". format(rssbot.max_feeds))
+print(banner.rssbot_logo)
+print("--------------------------")
+print("Title: {}". format(rssbot.title))
+print("Agent: {}". format(rssbot.user_agent))
+print("Max Feeds: {}". format(rssbot.max_feeds))
 
-    # iterations
-    i = 0
-    # links processed
-    p = 0
-    # links added
-    a = 0
-    if rssbot.feed_count > 0:
-        print("Aggregating {} Feeds". format(rssbot.feeds_count))
+# iterations
+i = 0
+# links processed
+p = 0
+# links added
+a = 0
+if rssbot.feed_count > 0:
+    print("Aggregating {} Feeds". format(rssbot.feeds_count))
 
-        for feed in rssbot.feeds:
-            rss = feedparser.parse(feed[1], referrer=rssbot.root_url)
-            if rssbot.debug:
-                print("--------------------------")
-                if 'title' in rss.feed:
-                    print("RSS Feed Title: {}". format(rss.feed.title))
-                if 'link' in rss.feed:
-                    print("RSS Feed URL: {}". format(rss.feed.link))
-            try:
-                rss.status
-            except ValueError:
-                print('HTTP Status not found.')
+    for feed in rssbot.feeds:
+        rss = feedparser.parse(feed[1], referrer=rssbot.root_url)
+        if rssbot.debug:
+            print("--------------------------")
+            if 'title' in rss.feed:
+                print("RSS Feed Title: {}". format(rss.feed.title))
+            if 'link' in rss.feed:
+                print("RSS Feed URL: {}". format(rss.feed.link))
+        try:
+            rss.status
+        except ValueError:
+            print('HTTP Status not found.')
+        else:
+            http_status = "HTTP Response Status Code: %d" % (rss.status)
+            print(http_status)
+            if 'title' in rss.feed:
+                if len(rss.entries) > 0:
+                    for entry in rss.entries:
+                        if 'title' in entry and 'link' in entry:
+                            id = rssbot.add_link(feed[2], entry.title, entry.link)
+                            try:
+                                if id > 0:
+                                    if rssbot.debug:
+                                        print("\nAdded Record {}". format(id))
+                                        print(str(entry.title))
+                                        print(str(entry.link))
+                                    a = a + 1
+                            except ValueError:
+                                print("Exception thrown while processing item.")
+                            p = p + 1
+                        else:
+                            print('Unable to parse item. Bad title or link format.')
             else:
-                http_status = "HTTP Response Status Code: %d" % (rss.status)
-                print(http_status)
-                if 'title' in rss.feed:
-                    if len(rss.entries) > 0:
-                        for entry in rss.entries:
-                            if 'title' in entry and 'link' in entry:
-                                id = rssbot.add_link(feed[2], entry.title, entry.link)
-                                try:
-                                    if id > 0:
-                                        if rssbot.debug:
-                                            print("\nAdded Record {}". format(id))
-                                            print(str(entry.title))
-                                            print(str(entry.link))
-                                        a = a + 1
-                                except ValueError:
-                                    print("Exception thrown while processing item.")
-                                p = p + 1
-                            else:
-                                print('Unable to parse item. Bad title or link format.')
-                else:
-                    rssbot.deactivate_feed(feed[2])
-            i = i + 1
-    print("--------------------------")
-    print("Processed feeds: {}". format(i))
-    print("Processed items: {}". format(p))
-    print("Added new links: {}". format(a))
-    rssbot.conn.close()
+                rssbot.deactivate_feed(feed[2])
+        i = i + 1
 
-parse_feeds(rssbot)
+print("--------------------------")
+print("Processed feeds: {}". format(i))
+print("Processed items: {}". format(p))
+print("Added new links: {}". format(a))
+
+rssbot.conn.close()
